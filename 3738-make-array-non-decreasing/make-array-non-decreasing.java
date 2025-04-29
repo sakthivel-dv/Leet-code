3818167d57d018f352cf4ad6789c68d0f03f2583
sakthivel-dv <dsakthivel671@gmail.com>
2025-04-29 20:31:07 +0530

    Time: 3 ms (64.49%) | Memory: 73.3 MB (25.72%) - LeetSync

---
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumPossibleSize(int[] nums) {
+     int prev=-1,size=0;  
+     for(int num:nums)
+        if(num>=prev)
+        {prev=num; size++;}
+      
+     return size;
+    }
+}
