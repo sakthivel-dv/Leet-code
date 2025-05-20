b9525d008969e4d31be8a6e4d8f11ce427fe2304
sakthivel-dv <dsakthivel671@gmail.com>
2025-05-20 17:10:32 +0530

    Time: 1 ms (98.51%) | Memory: 42.4 MB (84.45%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public void helper(TreeNode root, ArrayList<String> result, StringBuilder sb){
+        if(root==null){
+            return;
+        }
+        int length=sb.length();
+        if(root.left==null&&root.right==null){
+            sb.append(root.val);
+            result.add(sb.toString());
+            sb.setLength(length);
+            return;
+        }
+        sb.append(root.val);
+        sb.append("->");
+        helper(root.left,result,sb);
+        helper(root.right,result,sb);
+        sb.setLength(length);
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+    ArrayList<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        helper(root, result, sb);
+        
+        return result;
+    }
+}
