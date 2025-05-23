ec8c3fde724e0190c8718f9b1302e5ab6fa4a4de
sakthivel-dv <dsakthivel671@gmail.com>
2025-05-23 15:43:25 +0530

    Time: 1 ms (40.00%) | Memory: 41.5 MB (59.34%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String sortSentence(String s) {
+      String[] words = s.split(" ");
+        String[] sorted = new String[words.length];
+
+        for (String word : words) {
+            int index = Character.getNumericValue(word.charAt(word.length() - 1)) - 1;
+            sorted[index] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", sorted);   
+    }
+}
