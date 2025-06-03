1fabdbed6b45e9c735974e6d6368a9a183e778e4
sakthivel-dv <dsakthivel671@gmail.com>
2025-06-03 14:36:55 +0530

    Time: 1 ms (92.47%) | Memory: 41.7 MB (94.39%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+     int caps=0,low=0,first=0;
+     for(char ch:word.toCharArray()){
+        if(ch>=65&&ch<=90){
+            caps++;
+            if(first==0)
+            first=1;
+        }
+        else{
+            low++;
+            if(first==0)
+            first=2;
+        }
+     }   
+     if(caps==0||low==0||first==1&&caps==1)
+     return true;
+     else
+     return false;
+    }
+}
