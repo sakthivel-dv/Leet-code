a019369345727172787bdb457e4837ff09d0b1ea
sakthivel-dv <dsakthivel671@gmail.com>
2025-06-09 10:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (63.75%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,13 +1,10 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-     int val[]= new int[101];
-
-        int sum=0;
-
-        for(int i=0; i<nums.length; i++){
-            sum+= val[nums[i]]++;
-        }
-
-        return sum ;   
+    int val[]=new int[101];
+    int sum=0;
+    for(int i=0;i<nums.length;i++){
+     sum+=val[nums[i]]++;   
+    }    
+    return sum;
     }
 }
